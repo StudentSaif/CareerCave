6696e51cca0be67acff41ad5339414af74d37bbc
unknown <anujreoti7398@gmail.com>
2025-01-11 16:16:03 +0530

    anuj changes occur

--- resources/views/frontend/home.blade.php
+++ resources/views/frontend/home.blade.php
@@ -1,7 +1,7 @@
 @extends('frontend.layouts.master')
 
 @section('content')
-<section class="section-0 lazy d-flex bg-image-style dark align-items-center " class=""
+    <section class="section-0 lazy d-flex bg-image-style dark align-items-center " class=""
         data-bg="assets/images/banner5.jpg">
         <div class="container">
             <div class="row">
@@ -461,29 +461,4 @@
             </div>
         </div>
     </section>
-    <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel"
-        aria-hidden="true">
-        <div class="modal-dialog modal-dialog-centered">
-            <div class="modal-content">
-                <div class="modal-header">
-                    <h5 class="modal-title pb-0" id="exampleModalLabel">Change Profile Picture</h5>
-                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                </div>
-                <div class="modal-body">
-                    <form>
-                        <div class="mb-3">
-                            <label for="exampleInputEmail1" class="form-label">Profile Image</label>
-                            <input type="file" class="form-control" id="image" name="image">
-                        </div>
-                        <div class="d-flex justify-content-end">
-                            <button type="submit" class="btn btn-primary mx-3">Update</button>
-                            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-                        </div>
-
-                    </form>
-                </div>
-            </div>
-        </div>
-    </div>
-
 @endsection
